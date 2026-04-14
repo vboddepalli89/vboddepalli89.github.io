a9145b83bb85045939e47a27a917202dd663fd44
vboddepalli89 <126285608+vboddepalli89@users.noreply.github.com>
2026-04-14 02:54:05 +0000

    Deploying to gh-pages from @ vboddepalli89/vboddepalli89.github.io@9e887485cb8ec9467b210520543d0cc935ea900e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -478,21 +478,21 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Naresh_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%61%72%65%73%68.%68%6F%72%74%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/boddepalli89", "_blank");
         },
       },{
         id: 'social-rss',
@@ -506,14 +506,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=wscgEDkAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
